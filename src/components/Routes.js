2f96eaa7cab8a11080e2e7ad463ca5be7bafebdd
Gabriel B Quintáns <gabrielbarreiroquintans@gmail.com>
2018-12-05 09:44:52 +0100

    Modified Routes.js to go secret-dinner page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,12 +3,14 @@ import { Router } from "@reach/router";
 import StorePicker from "./StorePicker";
 import App from "./App";
 import NotFound from "./NotFound";
+import Pizza from "./Pizza";
 
 const Routes = () => (
   <Router>
     <StorePicker path="/" />
     <App path="/store/:storeId" />
     {/* TODO: Hook up the new Pizza component to render @ "/secret-dinner" */}
+    <Pizza path="/secret-dinner" />
     {/* HINT: You need to write code 👇 and also 👆 */}
     <NotFound default />
   </Router>
